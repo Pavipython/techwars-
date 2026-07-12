c4155986df0a43c3234dca5344e9f8804b7a8893
Pavipython <9002ivap@gmail.com>
2026-07-12 14:48:17 +0100

    Add files via upload

--- multiplayer.py
+++ multiplayer.py
@@ -7,15 +7,35 @@ bg=pygame.transform.scale(bg,(W,H))
 cyborg=pygame.transform.scale(pygame.image.load("Cyborg.png"),(300,300))
 golem=pygame.transform.scale(pygame.image.load("golem.png"),(300,300))
 
-def draw():
+
+def draw(cr,gr):
     techwars.blit(bg,(0,0))
-    techwars.blit(cyborg,(100,H/2))
-    techwars.blit(golem,(W-400,H/2))
+    # pygame.draw.rect(techwars,"cyan",cr)
+    # pygame.draw.rect(techwars,"cyan",gr)
+    techwars.blit(cyborg,(cr.x-60,cr.y-10))
+    techwars.blit(golem,(gr.x-40,gr.y-20))
+
+def handlemovement(cr,gr,keys):
+    if keys [pygame.K_UP] and cr.y > 10:
+        cr.y-=10
+    if keys [pygame.K_DOWN] and cr.y + cr.height < H:
+        cr.y+=10
+    if keys [pygame.K_RIGHT]:
+        cr.x+=10
+    if keys [pygame.K_LEFT]:
+        cr.x-=10
+
+
 
 def main():
+    cr=pygame.Rect(100,H/2,180,280)
+    gr=pygame.Rect(W-400,H/2,220,260)
+
 
     while True:
-        draw()
+        draw(cr,gr)
+        keys=pygame.key.get_pressed()
+        handlemovement(cr,gr,keys)
         for i in pygame.event.get():
             print(i)
             if i.type==pygame.QUIT:
