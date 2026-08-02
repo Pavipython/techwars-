b516424bda52aa114f033fb1351b920b54cb56c0
Pavipython <9002ivap@gmail.com>
2026-08-02 14:47:43 +0100

    Add files via upload

--- multiplayer.py
+++ multiplayer.py
@@ -5,16 +5,25 @@ techwars=pygame.display.set_mode((W,H))
 bg=pygame.image.load("darkcity.jpg")
 bg=pygame.transform.scale(bg,(W,H))
 cyborg=pygame.transform.scale(pygame.image.load("Cyborg.png"),(300,300))
+cyborg=pygame.transform.flip(cyborg,True,False)
 golem=pygame.transform.scale(pygame.image.load("golem.png"),(300,300))
 border=pygame.Rect(W/2-25,0,50,H)
 fence=pygame.transform.scale(pygame.image.load("Fence.png"),(50,H))
-def draw(cr,gr):
+laser=pygame.transform.scale(pygame.image.load("laser.png"),(100,20))
+boulder=pygame.transform.scale(pygame.image.load("meteor1.png"),(80,60))
+def draw(cr,gr,lasers,boulders):
     techwars.blit(bg,(0,0))
     # pygame.draw.rect(techwars,"cyan",cr)
     # pygame.draw.rect(techwars,"cyan",gr)
     techwars.blit(cyborg,(cr.x-60,cr.y-10))
     techwars.blit(golem,(gr.x-40,gr.y-20))
     techwars.blit(fence,(border.x, border.y))
+    for i in lasers:
+        # pygame.draw.rect(techwars,"white", i)
+        techwars.blit(laser,(i.x, i.y))
+    for i in boulders:
+            # pygame.draw.rect(techwars,"brown", i)
+            techwars.blit(boulder,(i.x, i.y))    
 
 def handlemovement(cr,gr,keys):
     if keys [pygame.K_UP] and cr.y > 10:
@@ -32,22 +41,33 @@ def handlemovement(cr,gr,keys):
             gr.y +=random.randint(-100,100)
 
 
-
+def handlebullets(cr,gr,lasers,boulders):
+    pass
 
 
 def main():
     cr=pygame.Rect(100,H/2,180,280)
     gr=pygame.Rect(W-400,H/2,220,260)
+    lasers=[]
+    boulders=[]
 
 
     while True:
-        draw(cr,gr)
+        draw(cr,gr,lasers,boulders)
         keys=pygame.key.get_pressed()
         handlemovement(cr,gr,keys)
         for i in pygame.event.get():
             print(i)
             if i.type==pygame.QUIT:
                 pygame.quit()
+            if i.type==pygame.KEYDOWN:
+                if i.key==pygame.K_SPACE:
+                    bullet=pygame.Rect(cr.x + cr.width/2, cr.y + 50, 100,20)
+                    lasers.append(bullet)
+        if random.randint(1,100) < 5:
+            bullet=pygame.Rect(gr.x , gr.y + gr.height -100, 80,60)
+            boulders.append(bullet)
+        handlebullets(cr,gr,lasers,boulders)
         pygame.display.update()
 
 main()
--- multiplayer2.py
+++ multiplayer2.py
@@ -6,21 +6,24 @@ bg=pygame.image.load("Bluesky.jpg")
 bg=pygame.transform.scale(bg,(W,H))
 Ironman=pygame.transform.scale(pygame.image.load("Ironman.png"),(300,300))
 Thor=pygame.transform.scale(pygame.image.load("Thor.png"),(300,300))
+border=pygame.Rect(W/2-25,0,50,H)
+Tower=pygame.transform.scale(pygame.image.load("Tower.png"),(50,H))
 
 
 def draw(cr,gr):
     techwars.blit(bg,(0,0))
     techwars.blit(Ironman,(cr.x-60,cr.y-10))
     techwars.blit(Thor,(gr.x-40,gr.y-20))
+    techwars.blit(Tower,(border.x, border.y))
 
-def handlemovement(cr,gr,keys):
-    if keys [pygame.K_UP] and cr.y > 10:
+def handlemovement(ir,gr,keys):
+    if keys [pygame.K_UP] and ir.y > 10:
         cr.y-=6
     if keys [pygame.K_DOWN] and cr.y + cr.height < H:
         cr.y+=6
-    if keys [pygame.K_RIGHT]:
+    if keys [pygame.K_RIGHT] and cr.x + cr.width < border.x:
         cr.x+=6
-    if keys [pygame.K_LEFT]:
+    if keys [pygame.K_LEFT] and cr.x > 0:
         cr.x-=6
     if keys [pygame.K_w] and gr.y > 10:
         gr.y-=6
@@ -33,8 +36,8 @@ def handlemovement(cr,gr,keys):
     
 
 def main():
-    cr=pygame.Rect(100,H/2,180,280)
-    gr=pygame.Rect(W-400,H/2,220,260)
+    ir=pygame.Rect(100,H/2,180,280)
+    th=pygame.Rect(W-400,H/2,220,260)
 
 
 
